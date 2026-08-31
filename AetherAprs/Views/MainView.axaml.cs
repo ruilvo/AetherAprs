f1ec98ba30b9b8ec554ec6616f52e196156274bc
Rui Oliveira <ruimail24@gmail.com>
2026-08-31 18:36:32 +0100

    Cleanup MainView

--- AetherAprs/Views/MainView.axaml.cs
+++ AetherAprs/Views/MainView.axaml.cs
@@ -1,24 +1,14 @@
 // This file is part of AetherAprs
 // SPDX-FileCopyrightText: 2026 Rui Oliveira <ruimail24@gmail.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
-using Avalonia;
 using Avalonia.Controls;
-using AetherAprs.ViewModels;
 
 namespace AetherAprs.Views;
 
 public partial class MainView : UserControl
 {
-    public MainView() : this(null)
-    {
-    }
-
-    public MainView(MainViewModel? viewModel)
+    public MainView()
     {
         InitializeComponent();
-        if (viewModel is not null)
-        {
-            DataContext = viewModel;
-        }
     }
 }
