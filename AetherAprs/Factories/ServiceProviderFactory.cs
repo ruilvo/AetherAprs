b75952731223fd947442a24e65a3eb35dbbf0c3b
Rui Oliveira <ruimail24@gmail.com>
2026-09-01 20:58:43 +0100

    Cleanup service provider factory

--- AetherAprs/Factories/ServiceProviderFactory.cs
+++ AetherAprs/Factories/ServiceProviderFactory.cs
@@ -12,30 +12,20 @@ namespace AetherAprs.Factories;
 
 public static class ServiceProviderFactory
 {
-    private static readonly Lazy<IServiceProvider> serviceProvider = new(() =>
-    {
-        var services = new ServiceCollection();
-        ConfigureServices(services);
-        return services.BuildServiceProvider();
-    });
+    private static IServiceProvider? serviceProvider;
 
-    /// <summary>
-    /// Gets the configured service provider instance.
-    /// This is a singleton that will be created on first access.
-    /// </summary>
-    public static IServiceProvider ServiceProvider => serviceProvider.Value;
+    public static IServiceProvider ServiceProvider => serviceProvider ??= CreateServiceProvider(_ =>
+        throw new InvalidOperationException("A platform service configuration " +
+        "must be supplied before creating the application service provider.\n" +
+        "Call ServiceProviderFactory.CreateServiceProvider with a " +
+        "platform -specific configuration action before accessing the " +
+        "ServiceProvider property."));
 
-    /// <summary>
-    /// Configures the standard services for the application.
-    /// </summary>
-    /// <param name="services">The service collection to configure.</param>
-    public static void ConfigureServices(IServiceCollection services)
+    private static void ConfigureServices(IServiceCollection services)
     {
         // Register configuration
         ConfigurationService configService = new();
         services.AddSingleton<IConfigurationService>(configService);
-        services.AddSingleton(configService.Configuration);
-        services.AddSingleton(configService.Settings);
 
         // Register logging
         services.AddLogging(builder =>
@@ -49,17 +39,12 @@ public static class ServiceProviderFactory
         services.AddSingleton<MainViewModel>();
     }
 
-    /// <summary>
-    /// Creates a new service provider with optional additional configuration.
-    /// This is useful when you need platform-specific services or custom configuration.
-    /// </summary>
-    /// <param name="additionalConfiguration">Optional action to configure additional services.</param>
-    /// <returns>A configured service provider.</returns>
-    public static IServiceProvider CreateServiceProvider(Action<IServiceCollection>? additionalConfiguration = null)
+    public static IServiceProvider CreateServiceProvider(Action<IServiceCollection> configurePlatformServices)
     {
         var services = new ServiceCollection();
+        configurePlatformServices(services);
         ConfigureServices(services);
-        additionalConfiguration?.Invoke(services);
-        return services.BuildServiceProvider();
+        serviceProvider = services.BuildServiceProvider();
+        return serviceProvider;
     }
 }
